7b92e4094e57c7d955dbaf5697ef6882044ee97a
lightcaptainguy <130440682+lightcaptainguy@users.noreply.github.com>
2026-03-16 19:53:53 +0530

    Initial Structure and basic types defined
    Defined the basic structure and folder structure, defined the types ( weight, hypotheses, evidence )

---
+++ include/types.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <iostream>
+#include <string>
+
+enum class Weight {
+
+    HIGH_SUPPORT,
+    MEDIUM_SUPPORT,
+    LOW_SUPPORT,
+    NA,
+    LOW_REFUTE,
+    MEDIUM_REFUTE,
+    HIGH_REFUTE
+
+};
+
+struct Hypotheses {
+    std::string name;
+    float prior;
+    float posterior;
+};
+
+struct Evidence {
+    std::string description;
+    float credibility;
+
+}
---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include "include/types.h"
+#include <iostream>
+#include <vector>
+
+int main() {
+    //harcdoed scenario Taiwan-CHina
+    std::vector<Hypotheses> Hypotheses = {
+        
+    }
+}
