4029654134e8a19c811f1dce23157a9395e506e0
lightcaptainguy <130440682+lightcaptainguy@users.noreply.github.com>
2026-03-22 23:31:46 +0530

    Created Function signatures for the Bayesian header

--- include/bayesian.h
+++ include/bayesian.h
@@ -5,9 +5,13 @@
 // takes in - prior from hypotheses, and Probability P(B|Ahypotheses )
 // ( probability that B happens whenever hypotheses happens regardless)
 //outputs -  P(B) = Σ P(B|Ai) × P(Ai)
+double weightTolikelihood(Weight w); // helper
+double uncondprob(vector<Hypotheses>& hypotheses, vector<Weight>& Weights);
+
 
 //function 2 - calculation of posterior gices value to hypotheses struct
 //update all hypothesis posteriors given one piece of evidence
 // Takes in - P(B) from function 1, Prior of hypotheses ( only one this time)
 //probability of B conditioned that the taken hypotheses occurs regardless
 //Outputs - updates the posterior float and updates the table according to the evidence
+void posteriorvalue(vector<Hypotheses>& hypotheses, vector<Weight>& Weights, double probB);
